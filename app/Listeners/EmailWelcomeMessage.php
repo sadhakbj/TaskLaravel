0476f8b5f9e97f6c7788d7c9137eb62a58b1f381
sadhakbj <sadhakbj@gmail.com>
2017-01-26 17:30:10 +0545

    Update Email Template - [x] Added new blade files for email templates

--- app/Listeners/EmailWelcomeMessage.php
+++ app/Listeners/EmailWelcomeMessage.php
@@ -16,20 +16,12 @@ class EmailWelcomeMessage
     public function handle(UserWasRegistered $event)
     {
         $user    = auth()->user();
-        $content = sprintf(
-            'Welcome %s ! , 
-            you have successfully registered to our application. Please keep on checking the email for 
-            the latest updates',
-            $user->name
-        );
 
-        Mail::raw(
-            $content,
-            function ($m) use ($user) {
-                $m->from('admin@app.com', 'Task Laravel');
+        Mail::send('emails.welcome', ['user' => $user], function ($m) use ($user) {
+            $m->from('hello@app.com', 'Your Application');
 
-                $m->to($user->email)->subject('Welcome to TaskLaravel.');
-            }
-        );
+            $m->to($user->email, $user->name)->subject('Your Reminder!');
+        });
+
     }
 }
